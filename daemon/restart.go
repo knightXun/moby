865eb35c561590e9d61d5811f7d251a24963d4ce
knight <1004815462@qq.com>
2018-08-06 19:17:34 +0800

    when restart container,let restartcount++

--- daemon/restart.go
+++ daemon/restart.go
@@ -3,7 +3,13 @@ package daemon
 import (
 	"fmt"
 
+	"github.com/Sirupsen/logrus"
 	"github.com/docker/docker/container"
+	"github.com/docker/docker/libcontainerd"
+	"github.com/docker/docker/runconfig"
+	"google.golang.org/grpc"
+	"strings"
+	"syscall"
 )
 
 // ContainerRestart stops and starts a container. It attempts to
@@ -39,10 +45,91 @@ func (daemon *Daemon) containerRestart(container *container.Container, seconds i
 		return err
 	}
 
-	if err := daemon.containerStart(container); err != nil {
+	//container.RestartCount = container.RestartCount + 1
+	if err := daemon.restart(container); err != nil {
 		return err
 	}
 
 	daemon.LogContainerEvent(container, "restart")
 	return nil
+}
+
+func (daemon *Daemon) restart(container *container.Container) (err error) {
+	container.Lock()
+	defer container.Unlock()
+
+	restartCount := container.RestartCount + 1
+	if container.Running {
+		return nil
+	}
+
+	if container.RemovalInProgress || container.Dead {
+		return fmt.Errorf("Container is marked for removal and cannot be started.")
+	}
+
+	// if we encounter an error during start we need to ensure that any other
+	// setup has been cleaned up properly
+	defer func() {
+		if err != nil {
+			container.SetError(err)
+			// if no one else has set it, make sure we don't leave it at zero
+			if container.ExitCode() == 0 {
+				container.SetExitCode(128)
+			}
+			container.ToDisk()
+			daemon.Cleanup(container)
+		}
+	}()
+
+	if err := daemon.conditionalMountOnStart(container); err != nil {
+		return err
+	}
+
+	// Make sure NetworkMode has an acceptable value. We do this to ensure
+	// backwards API compatibility.
+	container.HostConfig = runconfig.SetDefaultNetModeIfBlank(container.HostConfig)
+
+	if err := daemon.initializeNetworking(container); err != nil {
+		return err
+	}
+
+	spec, err := daemon.createSpec(container)
+	if err != nil {
+		return err
+	}
+
+	createOptions := []libcontainerd.CreateOption{libcontainerd.WithRestartManager(container.RestartManager(true))}
+	copts, err := daemon.getLibcontainerdCreateOptions(container)
+	if err != nil {
+		return err
+	}
+	if copts != nil {
+		createOptions = append(createOptions, *copts...)
+	}
+
+	if err := daemon.containerd.Create(container.ID, *spec, createOptions...); err != nil {
+		errDesc := grpc.ErrorDesc(err)
+		logrus.Errorf("Create container failed with error: %s", errDesc)
+		// if we receive an internal error from the initial start of a container then lets
+		// return it instead of entering the restart loop
+		// set to 127 for container cmd not found/does not exist)
+		if strings.Contains(errDesc, container.Path) &&
+			(strings.Contains(errDesc, "executable file not found") ||
+				strings.Contains(errDesc, "no such file or directory") ||
+				strings.Contains(errDesc, "system cannot find the file specified")) {
+			container.SetExitCode(127)
+		}
+		// set to 126 for container cmd can't be invoked errors
+		if strings.Contains(errDesc, syscall.EACCES.Error()) {
+			container.SetExitCode(126)
+		}
+
+		container.Reset(false)
+
+		return fmt.Errorf("%s", errDesc)
+	}
+
+	container.RestartCount = restartCount
+	return container.ToDisk()
+
 }
